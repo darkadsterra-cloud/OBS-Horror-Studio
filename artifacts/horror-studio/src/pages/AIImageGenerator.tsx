d312246566f1ad6aedf03511ba445969da0298d7
darkadsterra-cloud <darkadsterra@gmail.com>
2026-04-25 04:25:53 +0500

    Create AIImageGenerator.tsx

---
+++ artifacts/horror-studio/src/pages/AIImageGenerator.tsx
@@ -0,0 +1,10 @@
+import ImagePromptPanel from "../components/ImagePromptPanel";
+
+export default function AIImageGenerator(){
+return(
+<div style={{padding:"30px"}}>
+<h1>AI Image Generator</h1>
+<ImagePromptPanel/>
+</div>
+);
+}
